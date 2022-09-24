70cf5385a4a7300f58ddd2098180c486c3a56a8d
Michael Thaler <michael.thaler@dlr.de>
2022-09-24 18:31:25 +0200

    Added FixedDepositDetails.

--- com/mthaler/bankapp/dao/FixedDepositDao.java
+++ com/mthaler/bankapp/dao/FixedDepositDao.java
@@ -1,5 +1,6 @@
 package com.mthaler.bankapp.dao;
 
+import com.mthaler.bankapp.domain.FixedDepositDetails;
 public interface FixedDepositDao {
 	int createFixedDeposit(FixedDepositDetails fdd);
 	FixedDepositDetails getFixedDeposit(int fixedDepositId);
